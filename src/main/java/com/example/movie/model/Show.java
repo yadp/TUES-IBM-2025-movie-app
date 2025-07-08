c4dbe2a6c50cb47488e32262cffee20b018ce4f3
martindoychinov07 <martin.v.doychinov.2021@elsys-bg.org>
2025-07-08 15:20:42 +0300

    Added show model

--- src/main/java/com/example/movie/model/Show.java
+++ src/main/java/com/example/movie/model/Show.java
@@ -1,5 +1,22 @@
 package com.example.movie.model;
 
-public class Show extends Media{
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NonNull;
 
+@Entity
+@DiscriminatorValue("Show")
+public class Show extends Media{
+    @NonNull
+    @Column(nullable = false)
+    @Getter @Setter
+    private int number_of_episodes;
+
+    @NonNull
+    @Column(nullable = false)
+    @Getter @Setter
+    private int number_of_seasons;
 }
