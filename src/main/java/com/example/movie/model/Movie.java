a7a01172f4e3e4b281f30c06b7f2391a35ee56a1
martindoychinov07 <martin.v.doychinov.2021@elsys-bg.org>
2025-07-08 15:09:17 +0300

    Updated movie model

--- src/main/java/com/example/movie/model/Movie.java
+++ src/main/java/com/example/movie/model/Movie.java
@@ -1,12 +1,15 @@
 package com.example.movie.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.NonNull;
 
+@Entity
 public class Movie extends Media{
     @NonNull
     @Column(nullable = false)
-    @Getter
+    @Getter @Setter
     private int duration;
 }
---
+++ src/main/java/com/example/movie/model/Show.java
@@ -0,0 +1,5 @@
+package com.example.movie.model;
+
+public class Show extends Media{
+
+}
