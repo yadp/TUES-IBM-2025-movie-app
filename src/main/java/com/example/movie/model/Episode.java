3bb4c1a41525dbcab5bc6d20e52531439eb24ffd
martindoychinov07 <martin.v.doychinov.2021@elsys-bg.org>
2025-07-09 10:36:15 +0300

    Added episode class

--- src/main/java/com/example/movie/model/Episode.java
+++ src/main/java/com/example/movie/model/Episode.java
@@ -1,15 +1,14 @@
 package com.example.movie.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
 
-@Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@Table(name = "media")
-@DiscriminatorColumn(name = "media_type")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@RequiredArgsConstructor
-public class Media {
+public class Episode {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
@@ -23,30 +22,25 @@ public class Media {
     @NonNull
     @Column(nullable = false)
     @Getter @Setter
-    private Short year;
-
-    @NonNull
-    @Column(nullable = false)
-    @Getter @Setter
     private String description;
 
     @NonNull
     @Column(nullable = false)
     @Getter @Setter
-    private String genre;
+    private Integer duration;
 
     @NonNull
     @Column(nullable = false)
     @Getter @Setter
-    private String type;
+    private Integer media_id;
 
     @NonNull
     @Column(nullable = false)
-    @Getter
-    private Float averageRating;
+    @Getter @Setter
+    private Integer season_number;
 
     @NonNull
     @Column(nullable = false)
-    @Getter
-    private Integer ratingsCount;
+    @Getter @Setter
+    private Integer episode_number;
 }
